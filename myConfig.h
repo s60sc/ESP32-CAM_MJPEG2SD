fe8e1bf1d5a6c7a1d61b1b24ac8fbe8f4771bde0
gemi254 <gemi254@192.168.56.1>
2020-06-29 13:28:29 +0300

    Add you custom config here

---
+++ myConfig.h
@@ -0,0 +1,8 @@
+#define MY_CONFIG
+
+const char* ssid = "router_myssid";
+const char* password = "router passwd";
+#define USE_PIR false // whether to use PIR for motion detection
+#define USE_MOTION true // whether to use camera for motion detection (with motionDetect.cpp)
+//#define TIMEZONE "GMT0BST,M3.5.0/01,M10.5.0/02" // set to local timezone 
+#define TIMEZONE "EET-2EEST-3,M3.5.0/03:00:00,M10.5.0/04:00:00" //Athens - Greece
