0d6fdbb46243e2f5d2d0bd441382aa04f58317f7
gemi254 <gemi254@192.168.56.1>
2020-06-30 15:29:41 +0300

    Define your settings here

--- myConfig.h
+++ myConfig.h
@@ -1,8 +1,15 @@
 #define MY_CONFIG
+//Add you configuration parameters here
 
-const char* ssid = "router_myssid";
-const char* password = "router passwd";
-#define USE_PIR false // whether to use PIR for motion detection
-#define USE_MOTION true // whether to use camera for motion detection (with motionDetect.cpp)
-//#define TIMEZONE "GMT0BST,M3.5.0/01,M10.5.0/02" // set to local timezone 
-#define TIMEZONE "EET-2EEST-3,M3.5.0/03:00:00,M10.5.0/04:00:00" //Athens - Greece
+//Wifi Station parameters
+const char* ssid = "router_myssid";           //Router ssid
+const char* password = "router_passwd"; //Router password
+//Time zone
+const char* TIMEZONE="GMT0BST,M3.5.0/01,M10.5.0/02"; 
+
+//Ftp upload parameters
+const char* ftp_server = "ftp_server";
+const char* ftp_port   = "21";
+const char* ftp_user   = "ftp_user";
+const char* ftp_pass   = "ftp_passwd";
+const char* ftp_wd     = "ftp_working_dir";
