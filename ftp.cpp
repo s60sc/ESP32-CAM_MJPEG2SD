835f721e2305177ab1018490d6c7e53d103ba1fe
gemi254 <gemi254@192.168.56.1>
2020-07-06 13:44:02 +0300

    Added timeout to ftp response

--- ftp.cpp
+++ ftp.cpp
@@ -6,8 +6,8 @@
 #include "SD_MMC.h"
 #include <vector>  // Dynamic string array
 
-//extern bool debug;
-bool dbg=1;
+#include "esp_log.h"
+static const char* TAG = "ftp";
 
 //Defined in custom config file myConfig.h
 extern const char* ftp_server;
@@ -19,9 +19,10 @@ extern const char* ftp_wd;
 unsigned int hiPort; //Data connection port
 
 //FTP buffers
-char outBuf[128];
-char outCount;
+char rspBuf[255]; //Ftp response buffer
+char rspCount;
 #define BUFF_SIZE 32 * 1024 // Upload data buffer size
+#define RESPONSE_TIMEOUT 10000
 //WiFi Clients
 WiFiClient client;
 WiFiClient dclient;
@@ -29,34 +30,40 @@ WiFiClient dclient;
 void efail(){
   byte thisByte = 0;
   client.println(F("QUIT"));
-  while (!client.available()) delay(1);
+  
+  unsigned long start = millis();
+  while (!client.available() && millis() < start + RESPONSE_TIMEOUT) delay(1);  
+
   while (client.available()) {
     thisByte = client.read();
     Serial.write(thisByte);
   }
   client.stop();
-  if(dbg) Serial.println("Ftp command disconnected");
+  ESP_LOGI(TAG, "Ftp command disconnected");  
 }
+
 
 byte eRcv(bool bFail=true){
   byte respCode;
   byte thisByte;
 
-  while (!client.available()) delay(1);
- 
+  unsigned long start = millis();
+  while (!client.available() && millis() < start + RESPONSE_TIMEOUT) delay(1);
+  
   respCode = client.peek();
-  outCount = 0;
+  rspCount = 0;
   while (client.available()) {
-    thisByte = client.read();
-    if(dbg) Serial.write(thisByte);
-    if (outCount < 127)  {
-      outBuf[outCount] = thisByte;
-      outCount++;
-      outBuf[outCount] = 0;
+    thisByte = client.read();    
+    if (rspCount < sizeof(rspBuf)) {       // if (rspCount < 127)  {
+      rspBuf[rspCount] = thisByte;
+      rspCount++;
+      rspBuf[rspCount] = 0;
     }
   }
-  //Serial.printf("respCode: %i\n", respCode);
-  if (respCode >= '4') {
+  //Skip line feed at end
+  rspBuf[rspCount-1]=0;
+  ESP_LOGI(TAG, "Ftp resp: %s",rspBuf);  
+  if (respCode >= '4' || respCode >= '5' ) {
     if(bFail) efail();
     return 0;
   }
@@ -67,9 +74,9 @@ byte eRcv(bool bFail=true){
 bool ftpConnect(){
   //Connect
   if (client.connect(ftp_server, String(ftp_port).toInt()) ) {
-    if(dbg) Serial.println(F("Ftp command connected"));
+    ESP_LOGI(TAG, "Ftp command connected");
   } else {
-    Serial.printf("Error opening ftp connection to %s %s\n", ftp_server,ftp_port);    
+    ESP_LOGE(TAG, "Error opening ftp connection to %s %s\n", ftp_server, ftp_port);
     return 0;
   }
   if (!eRcv()) return 0;
@@ -91,25 +98,24 @@ bool ftpConnect(){
   client.println("PASV");
   if (!eRcv()) return 0;
 
-  char *tStr = strtok(outBuf, "(,");
+  char *tStr = strtok(rspBuf, "(,");
   int array_pasv[6];
   for ( int i = 0; i < 6; i++) {
     tStr = strtok(NULL, "(,");
     array_pasv[i] = atoi(tStr);
     if (tStr == NULL)
     {
-      Serial.println(F("Bad PASV Answer"));
+      ESP_LOGE(TAG, "Bad PASV Answer");
       return 0;
     }
   }
   unsigned int loPort;
   hiPort = array_pasv[4] << 8;
   loPort = array_pasv[5] & 255;
-  if(dbg) Serial.print(F("Data port: "));
+  //if(dbg) Serial.print(F("Data port: "));
   hiPort = hiPort | loPort;
-  if(dbg) Serial.println(hiPort);
-  
-  if(dbg) Serial.printf("Change to root dir: %s\n", ftp_wd);
+  ESP_LOGI(TAG, "Data port: %i",hiPort);
+  ESP_LOGI(TAG, "Change to root dir: %s", ftp_wd);
   client.print("CWD ");
   client.println(ftp_wd);
   if (!eRcv()){
@@ -125,7 +131,7 @@ byte ftpDisconnect(){
   if (!eRcv()) return 0;
 
   client.stop();
-  if(dbg) Serial.println(F("Ftp command disconnected"));
+  ESP_LOGI(TAG, "Ftp command disconnected");
 
   return 1;
 }
@@ -135,23 +141,23 @@ bool ftpCheckDirPath(String filePath, String &fileName){
   int lv=0;
   std::vector<String> dirPaths;  // declare dir tree paths
   for (char* token = strtok((char *)filePath.c_str(), "/"); token; token = strtok(NULL, "/")){
-      //Serial.printf("token=%s\n", token);
       dirPaths.push_back(token);
       lv++;
   }
   
   //Create sub dirs
   for(int i=lv-2; i>=0; --i){
-      if(dbg) Serial.printf("Checking sub dir[%i]: %s\n",i,dirPaths[i].c_str() );      
+      
+      ESP_LOGI(TAG, "Searching for sub dir[%i]: %s",i,dirPaths[i].c_str() );      
       client.print("CWD ");
       client.println(dirPaths[i].c_str());
       eRcv(false); //Don't stop if dir not exists
 
       //Create dir if not exists
-      char *tStr = strtok(outBuf, " ");
+      char *tStr = strtok(rspBuf, " ");
       //Serial.printf("Res: %s\n",tStr);
       if(strcmp(tStr,"550")==0){
-        if(dbg) Serial.printf("Create dir: %s\n", dirPaths[i].c_str());
+        ESP_LOGI(TAG, "Create dir: %s", dirPaths[i].c_str());
         client.print("MKD ");        
         client.println(dirPaths[i].c_str());
         if (!eRcv()){
@@ -159,7 +165,7 @@ bool ftpCheckDirPath(String filePath, String &fileName){
           return 0;
        } 
        //Change to new dir
-       if(dbg) Serial.printf("Change to dir: %s\n", dirPaths[i].c_str());
+       ESP_LOGI(TAG, "Change to dir: %s", dirPaths[i].c_str());
        client.print("CWD ");
        client.println(dirPaths[i].c_str());
        if (!eRcv()){
@@ -174,14 +180,15 @@ bool ftpCheckDirPath(String filePath, String &fileName){
 }
 
 //Store sdfile to current ftp dir
-bool ftpStoreFile(String file, File &fh){
-  if(dbg) Serial.printf("Ftp store file: %s\n", file.c_str());
+bool ftpStoreFile(String file, File &fh){  
+  uint32_t fileSize = fh.size();
+  ESP_LOGI(TAG, "Ftp store file: %s size: %0.1fMB", file.c_str(),(float)(fileSize/(1024*1024)));
   
   //Connect to data port
   if (dclient.connect(ftp_server, hiPort)) {
-    if(dbg) Serial.println("Ftp data connected");
+    ESP_LOGI(TAG, "Ftp data connected");
   } else{
-    Serial.println("Ftp data connection failed");   
+    ESP_LOGE(TAG, "Ftp data connection failed");   
     return 0;
   }
   client.print("STOR ");
@@ -191,42 +198,47 @@ bool ftpStoreFile(String file, File &fh){
     return 0;
   }
   
-  if(dbg) Serial.println(F("Uploading.."));
+  ESP_LOGI(TAG, "Uploading..");
   //byte clientBuf[BUFF_SIZE];
   byte *clientBuf = (byte*)ps_malloc(BUFF_SIZE * sizeof(byte)); 
   if(clientBuf==NULL){
-    Serial.println(F("Memory allocation failed .."));
+    ESP_LOGE(TAG, "Memory allocation failed ..");
     dclient.stop();
     return 0;
   }
   
   unsigned int buffCount=0;
+  uint32_t writeBytes=0;
   unsigned long uploadStart = millis();
   size_t readLen,writeLen = 0;
   while (fh.available()){
     readLen = fh.read(clientBuf, BUFF_SIZE);
     if(readLen)  writeLen = dclient.write((const uint8_t *)clientBuf, readLen);
     if(readLen>0 && writeLen==0){
-        Serial.println(F("Write buffer failed .."));
+        ESP_LOGE(TAG, "Write buffer failed ..");
         dclient.stop();
         return 0;
     }
-    if(dbg && (++buffCount)%80==0) Serial.println("."); else Serial.print(F("."));
-    //Serial.printf("Wifi client write %u Kb\n", (writeLen / 1024));
+    writeBytes += writeLen;
+    if(buffCount%5==0){
+      ESP_LOGI(TAG, "Uploaded %0.0f%%", ( (double)writeBytes / fileSize)*100.0f );      
+    }
+    ++buffCount;
   }
+  if(readLen<BUFF_SIZE) ESP_LOGI(TAG, "Uploaded 100%%");
   float uploadDur =  (millis() - uploadStart)/1024;  
   free(clientBuf);
-  Serial.printf("\nDone Uploaded in %3.1f sec\n",uploadDur); 
+  ESP_LOGI(TAG, "Done Uploaded in %3.1f sec",uploadDur); 
   dclient.stop();
   return 1; 
 }
 
 //Upload a single file or whole directory to ftp 
 void uploadFolderOrFileFtp(String sdName, const bool removeAfterUpload, uint8_t levels){
-  if(dbg) Serial.printf("Ftp upload name: %s\n", sdName.c_str());
+  ESP_LOGI(TAG, "Ftp upload name: %s", sdName.c_str());
   if(sdName=="/"){
-      Serial.printf("Root is not allowed %s\n",sdName.c_str());  
-      return;  
+     ESP_LOGE(TAG, "Root is not allowed %s",sdName.c_str());  
+     return;  
   }
   String ftpName = "";
   
@@ -237,38 +249,38 @@ void uploadFolderOrFileFtp(String sdName, const bool removeAfterUpload, uint8_t 
 
   File root = SD_MMC.open(sdName);
   if (!root) {
-      Serial.printf("Failed to open: %s\n", sdName.c_str());
+      ESP_LOGE(TAG, "Failed to open: %s", sdName.c_str());
       ftpDisconnect();
       return;
   }  
     
   if (!root.isDirectory()) { //Upload a single file
-      if(dbg) Serial.printf("Uploading file: %s\n", sdName.c_str());    
+      ESP_LOGI(TAG, "Uploading file: %s", sdName.c_str());    
  
       if(!ftpCheckDirPath(sdName, ftpName)){
-          if(dbg) Serial.printf("Create ftp dir path %s failed\n", sdName.c_str());
+          ESP_LOGE(TAG, "Create ftp dir path %s failed", sdName.c_str());
           ftpDisconnect();
           return;
       }
       if(!ftpStoreFile(ftpName, root)){
-        Serial.printf("Store file %s to ftp failed\n", ftpName.c_str());
+        ESP_LOGE(TAG, "Store file %s to ftp failed", ftpName.c_str());
         ftpDisconnect();
         return;
       }
       root.close();
-      if(dbg) Serial.println(F("File closed"));
+      ESP_LOGV(TAG, "File closed");
       
       if(removeAfterUpload){
-        if(dbg) Serial.printf("Removing file %s\n", sdName.c_str()); 
+        ESP_LOGI(TAG, "Removing file %s", sdName.c_str()); 
         SD_MMC.remove(sdName.c_str());
       }    
   }else{  //Upload a whole directory
-      if(dbg) Serial.printf("Uploading directory: %s\n", sdName.c_str()); 
+      ESP_LOGI(TAG, "Uploading directory: %s", sdName.c_str()); 
       File fh = root.openNextFile();      
       sdName = fh.name();
       
       if(!ftpCheckDirPath(sdName, ftpName)){
-          Serial.printf("Create ftp dir path %s failed\n", sdName.c_str());
+          ESP_LOGE(TAG, "Create ftp dir path %s failed", sdName.c_str());
           ftpDisconnect();
           return;
       }
@@ -278,7 +290,7 @@ void uploadFolderOrFileFtp(String sdName, const bool removeAfterUpload, uint8_t 
           sdName = fh.name();
           bUploadOK = false;
           if (fh.isDirectory()) {
-              Serial.printf("Sub directory: %s, Not uploading\n", sdName.c_str());
+              ESP_LOGI(TAG, "Sub directory: %s, Not uploading", sdName.c_str());
               /*if (levels) {
                 std::string strfile(file.name());
                 const char* f="TEST";
@@ -287,15 +299,15 @@ void uploadFolderOrFileFtp(String sdName, const bool removeAfterUpload, uint8_t 
           } else {        
               byte bPos = sdName.lastIndexOf("/");
               String ftpName = sdName.substring(bPos+1);          
-              if(dbg) Serial.printf("Uploading sub sd file %s to %s\n", sdName.c_str(),ftpName.c_str()); 
+              ESP_LOGI(TAG, "Uploading sub sd file %s to %s", sdName.c_str(),ftpName.c_str()); 
               bUploadOK = ftpStoreFile(ftpName, fh);
               if(!bUploadOK){
-                Serial.printf("Store file %s to ftp failed\n", ftpName.c_str());                
+                ESP_LOGE(TAG, "Store file %s to ftp failed", ftpName.c_str());
               }
           }
           //Remove if ok
           if(removeAfterUpload && bUploadOK){
-            if(dbg) Serial.printf("Removing file %s\n", sdName.c_str()); 
+            ESP_LOGI(TAG, "Removing file %s\n", sdName.c_str()); 
             SD_MMC.remove(sdName.c_str());
           }
           fh = root.openNextFile();
@@ -307,15 +319,14 @@ void uploadFolderOrFileFtp(String sdName, const bool removeAfterUpload, uint8_t 
 
 static void taskUpload(void * parameter){
     String fname = (char *)parameter;
-    Serial.printf("Entering upload task with %s\n",fname.c_str());    
+    ESP_LOGV(TAG, "Entering upload task with %s\n",fname.c_str());    
     uploadFolderOrFileFtp(fname,false,0);
-    Serial.println("Ending uploadTask");
-    vTaskDelete( NULL );
-  
+    ESP_LOGV(TAG, "Ending uploadTask");
+    vTaskDelete( NULL );  
 }
 
 void createUploadTask(const char* val){
-    Serial.printf("Starting upload task with %s\n",val);
+    ESP_LOGV(TAG, "Starting upload task with %s\n",val);
     xTaskCreate(
         &taskUpload,       /* Task function. */
         "taskUpload",     /* String with name of task. */
@@ -323,5 +334,4 @@ void createUploadTask(const char* val){
         (void *)val,      /* Parameter passed as input of the task */
         1,                /* Priority of the task. */
         NULL);            /* Task handle. */
-
 }
