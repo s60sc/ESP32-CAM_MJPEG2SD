66f602cbf0f967b1aab8b70f6be348b0765f97dc
gemi254 <gemi254@192.168.56.1>
2020-07-11 12:38:29 +0300

    Fixed avi convert bug

--- app_httpd.cpp
+++ app_httpd.cpp
@@ -22,7 +22,8 @@
 #define PART_BOUNDARY "123456789000000000000987654321"
 static const char* _STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
 const char* _STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
-const char* _STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
+const char* _STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: %10u\r\n\r\n";
+
 
 httpd_handle_t stream_httpd = NULL;
 httpd_handle_t camera_httpd = NULL;
