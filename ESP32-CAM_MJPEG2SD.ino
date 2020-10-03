e18a4ec3ddaf82b2e08bedd13f6826f1f75e122c
gemi254 <gemi254@192.168.56.1>
2020-10-03 14:11:33 +0300

    Fixed

--- ESP32-CAM_MJPEG2SD.ino
+++ ESP32-CAM_MJPEG2SD.ino
@@ -4,7 +4,7 @@
 // WARNING!!! Make sure that you have either selected ESP32 Wrover Module,
 //            or another board which has PSRAM enabled
 //
-// Select camera model
+// Select camera model
 //#define CAMERA_MODEL_WROVER_KIT
 //#define CAMERA_MODEL_ESP_EYE
 //#define CAMERA_MODEL_M5STACK_PSRAM
