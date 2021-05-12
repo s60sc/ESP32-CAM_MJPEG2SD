912f74b6b440f1b09b08dc3d97b5ef1885dc2fd2
gemi254 <gemi254@192.168.56.1>
2021-05-12 15:01:38 +0300

    Compiled binary for ota or esptool, Version 2.2

--- ESP32-CAM_MJPEG2SD.ino
+++ ESP32-CAM_MJPEG2SD.ino
@@ -76,7 +76,7 @@ void setup() {
   config.pin_sscb_scl = SIOC_GPIO_NUM;
   config.pin_pwdn = PWDN_GPIO_NUM;
   config.pin_reset = RESET_GPIO_NUM;
-  config.xclk_freq_hz = 10000000;
+  config.xclk_freq_hz = XCLK_MHZ*1000000;
   config.pixel_format = PIXFORMAT_JPEG;
   //init with high specs to pre-allocate larger buffers
   if (psramFound()){
